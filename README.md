38e753161b747c0577238ec16478d158afa10eae
Joakim Svensberg <jockesv@gmail.com>
2020-01-27 21:24:08 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# jsreport_lab
+# Lab med JSReport
+
+Starta JSReport docker
+
+    $ docker run -d -p 5488:5488 jsreport/jsreport:2.6.1
+
+
+# Lab med NextCloud
+
+Starta NextCloud
+
+    $ docker run -d -p 8080:80 nextcloud
---
+++ client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using jsreport.Client;
+using jsreport.Types;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1().Wait();
+        }
+
+        public async static Task Test1()
+        {
+            var bodyTemplate = ReadTemplate(Path.Combine("templates", "hello.html"));
+            var footerTemplate = ReadTemplate(Path.Combine("templates", "footer.html"));
+
+            var rs = new ReportingService("http://localhost:5488");
+
+            var report = await rs.RenderAsync(new RenderRequest()
+            {
+                Template = new Template()
+                {
+                    Recipe = Recipe.ChromePdf,
+                    Engine = Engine.Handlebars,
+                    Content = bodyTemplate,
+                    Chrome = new Chrome {
+                        DisplayHeaderFooter = true,
+                        FooterTemplate = footerTemplate,
+                        Format = "A4",
+                        MarginBottom = "80px",
+                        MarginLeft = "20px",
+                        MarginRight = "20px",
+                        MarginTop = "20px"
+                    }
+                },
+                Data = new
+                {
+                    message = "world!"
+                }
+            });
+            
+            using (var file = File.Create("test.pdf"))
+            {
+                report.Content.Seek(0, SeekOrigin.Begin);
+                report.Content.CopyTo(file);
+            }
+        }
+
+        static string ReadTemplate(string filename)
+        {
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
